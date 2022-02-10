240b25a5dc59809dc8d8dd30f133d1a8c54b6433
Mark Elliot <mark.elliot@gmail.com>
2022-02-10 13:29:53 +0000

    format

--- gradle-versions/src/test/java/com/markelliot/gradle/versions/api/DependencyUpdateRecTests.java
+++ gradle-versions/src/test/java/com/markelliot/gradle/versions/api/DependencyUpdateRecTests.java
@@ -7,13 +7,11 @@ import org.junit.jupiter.api.Test;
 final class DependencyUpdateRecTests {
     @Test
     void test_handleEmptyCurrentVersion() {
-        DependencyUpdateRec rec = YamlSerDe.deserialize(
-            "group: group\n"
-                + "name: name\n"
-                + "latestVersion: latestVersion\n",
-            DependencyUpdateRec.class);
+        DependencyUpdateRec rec =
+                YamlSerDe.deserialize(
+                        "group: group\n" + "name: name\n" + "latestVersion: latestVersion\n",
+                        DependencyUpdateRec.class);
 
         assertThat(rec.currentVersion()).isNullOrEmpty();
     }
-
 }
