c9e21901e9b07f6dddcacb2a6b21b2f186714cad
Robert Kruszewski <robertk@palantir.com>
2022-03-31 19:04:38 +0100

    Fix: add test for single qoute version matching (#41)

--- gradle-versions/src/test/java/com/markelliot/gradle/versions/UpdatePluginsTaskTests.java
+++ gradle-versions/src/test/java/com/markelliot/gradle/versions/UpdatePluginsTaskTests.java
@@ -17,6 +17,13 @@ final class UpdatePluginsTaskTests {
                 .isEqualTo("id 'com.foo.bar' version \"0.2.0\"");
         assertThat(applyUpdate("id \"com.foo.bar\" version '0.1.0'", "com.foo.bar", "0.2.0"))
                 .isEqualTo("id \"com.foo.bar\" version '0.2.0'");
+        // trailing text
+        assertThat(
+                        applyUpdate(
+                                "id 'com.foo.bar' version '0.1.0'\nid 'com.foo.baz' version '0.1.0'",
+                                "com.foo.bar",
+                                "0.2.0"))
+                .isEqualTo("id 'com.foo.bar' version '0.2.0'\nid 'com.foo.baz' version '0.1.0'");
         // gradle/kotlin
         assertThat(applyUpdate("id(\"com.foo.bar\") version \"0.1.0\"", "com.foo.bar", "0.2.0"))
                 .isEqualTo("id(\"com.foo.bar\") version \"0.2.0\"");
