80ed9b1ee616002cf37f88b905a484010384b208
mellbot <mellbot@users.noreply.github.com>
2022-11-12 12:36:59 +0000

    Auto-update dependencies and plugins

--- gradle-versions/build.gradle.kts
+++ gradle-versions/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     `groovy`
     `java-gradle-plugin`
     `maven-publish`
-    id("com.gradle.plugin-publish") version "1.0.0"
+    id("com.gradle.plugin-publish") version "1.1.0"
 }
 
 dependencies {
